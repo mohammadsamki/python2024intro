8a982c7a441caad5188c864304a158643e74b1d6
mohammed alsamaki <samkimohd@gmail.com>
2024-05-04 17:44:19 +0300

    data type part 2

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-print('Hello World')
+# datatype in python
+name = 'ali nsjkasjkdajs'
+# print(name[3])
+name2= "sami dshjdfs\ngfgff"
+# print(name2)
+name3= """mohammad
+test
+ali
+ziad
+ahmad"""
+print(name3[9:12])
+tes='s,a,m'
+# print(tes[2])
+# tes[2]='a'
+# print(tes[2])
+
+list1 = [1,2,3,4,5,6,
+         7,8,9]
+# print(list1[4])
+list1[4]='test'
+# print(list1[4])
+# string int float complex  bool list tuple dict set
+complex1 = 5+2j #>
+#  what is j ?
+bool2=True
+bool1 =0
+
+print(bool1)
+print(bool2)
+bool2=10
+print(bool2)
+#  List
+list1 = [1,2,3,4,5,6,7,8,9]
+list1[5]='hghgh'
+#  tuble >
+tuble1 = (1,2,3,4,5,6,7,8,9)
+#
+dic1 = {'name':'ali','age':20}
+
+
+print(tuble1[5])
+# set
+set1 = {1,2,'3',4,5,6,7,8,9} # set
+print(set1)
+
